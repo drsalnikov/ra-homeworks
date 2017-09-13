cb722b1e172a8d6836833b50a10e48106c7626b3
salnikovev <drsalnikovev@gmail.com>
2017-09-13 22:38:07 +0300

    feedback done

--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,36 +1,49 @@
 'use strict';
 
 const FeedbackForm = ({data, onSubmit}) => {
-  console.log(data);
+
+  let form;
+  let wantToGet = {};
+
+  for (let snack of data.snacks) {
+    wantToGet[snack] = true;
+  }
 
   const myOnSubmit = (event) => {
     event.preventDefault();
-    //console.log(event);
-    console.dir(event.target);
     
+    data.name = form.name.value;
+    data.subject = form.subject.value;
+    data.message = form.message.value;
+    data.email = form.email.value;
+    data.salutation = form.querySelector('input[type=radio]:checked').value;
+    data.snacks = [];
+
+    let snacks = form.querySelectorAll('input[type=checkbox]:checked');
+
+    for (let snack of snacks) {
+      data.snacks.push(snack.value);
+    }
+
     onSubmit(JSON.stringify(data));
   };
 
-  const onChange = (event) => {
-    
-  };
-
   return (
-    <form className='content__form contact-form' onSubmit={myOnSubmit} onChange={onChange}>
+    <form className='content__form contact-form' onSubmit={myOnSubmit} ref={elem => form = elem}>
       <div className='testing'>
         <p>Чем мы можем помочь?</p>
       </div>
       <div className='contact-form__input-group'>
-        <input className='contact-form__input contact-form__input--radio' id='salutation-mr' name='salutation' type='radio' value='Мистер'/>
+        <input className='contact-form__input contact-form__input--radio' id='salutation-mr' name='salutation' type='radio' value='Мистер' defaultChecked={data.salutation === 'Мистер'} />
         <label className='contact-form__label contact-form__label--radio' htmlFor='salutation-mr'>Мистер</label>
-        <input className='contact-form__input contact-form__input--radio' id='salutation-mrs' name='salutation' type='radio' value='Мисис'/>
+        <input className='contact-form__input contact-form__input--radio' id='salutation-mrs' name='salutation' type='radio' value='Мисис' defaultChecked={data.salutation === 'Мисис'} />
         <label className='contact-form__label contact-form__label--radio' htmlFor='salutation-mrs'>Мисис</label>
-        <input className='contact-form__input contact-form__input--radio' id='salutation-ms' name='salutation' type='radio' value='Мис'/>
+        <input className='contact-form__input contact-form__input--radio' id='salutation-ms' name='salutation' type='radio' value='Мис' defaultChecked={data.salutation === 'Мис'} />
         <label className='contact-form__label contact-form__label--radio' htmlFor='salutation-ms'>Мис</label>
       </div>
       <div className='contact-form__input-group'>
         <label className='contact-form__label' htmlFor='name'>Имя</label>
-        <input className='contact-form__input contact-form__input--text' id='name' name='name' type='text' defaultValue={data.name}/>
+        <input className='contact-form__input contact-form__input--text' id='name' name='name' type='text' defaultValue={data.name} />
       </div>
       <div className='contact-form__input-group'>
         <label className='contact-form__label' htmlFor='email'>Адрес электронной почты</label>
@@ -49,9 +62,9 @@ const FeedbackForm = ({data, onSubmit}) => {
       </div>
       <div className='contact-form__input-group'>
         <p className='contact-form__label--checkbox-group'>Хочу получить:</p>
-        <input className='contact-form__input contact-form__input--checkbox' id='snacks-pizza' name='snacks' type='checkbox' value='пицца'/>
+        <input className='contact-form__input contact-form__input--checkbox' id='snacks-pizza' name='snacks' type='checkbox' value='пицца' defaultChecked={wantToGet['пицца']} />
         <label className='contact-form__label contact-form__label--checkbox' htmlFor='snacks-pizza'>Пиццу</label>
-        <input className='contact-form__input contact-form__input--checkbox' id='snacks-cake' name='snacks' type='checkbox' value='пирог'/>
+        <input className='contact-form__input contact-form__input--checkbox' id='snacks-cake' name='snacks' type='checkbox' value='пирог' defaultChecked={wantToGet['пирог']} />
         <label className='contact-form__label contact-form__label--checkbox' htmlFor='snacks-cake'>Пирог</label>
       </div>
       <button className='contact-form__button' type='submit'>Отправить сообщение!</button>
