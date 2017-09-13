19f3096620a3593adfe9812fd24499ebc9d4adc3
salnikovev <drsalnikovev@gmail.com>
2017-09-13 22:56:34 +0300

    in progress

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const AuthForm = ({onAuth}) => {
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (typeof onAuth === 'function' ){
+      console.log(typeof onAuth);
+    }
+  }
+
+  const getName = event => {
+    const field = event.currentTarget;
+    console.log(field.value);
+  };
+
+  const getEmail = event => {
+    console.log(event.currentTarget);
+  };
+
+  return (
+    <form className='ModalForm' action='/404/auth/' method='POST' onSubmit={onSubmit}>
+      <div className='Input'>
+        <input required type='text' placeholder='Имя' name='name' defaultValue='test' onChange={getName} />
+        <label></label>
+      </div>
+      <div className='Input'>
+        <input type='email' placeholder='Электронная почта' email='email' defaultValue='test@test.ts' onChange={getEmail} />
+        <label></label>
+      </div>
+      <div className='Input'>
+        <input required type='password' placeholder='Пароль' password='password' defaultValue='test'/>
+        <label></label>
+      </div>
+      <button type='submit'>
+        <span>Войти</span>
+        <i className='fa fa-fw fa-chevron-right'></i>
+      </button>
+    </form>
+  );
+};
