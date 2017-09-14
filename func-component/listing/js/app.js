0d1d9487dabed9449e6f0020a05826eb055faa6d
drsalnikov <drsalnikovev@gmail.com>
2017-09-14 16:39:48 +0300

    refactoring in progress

--- func-component/listing/js/app.js
+++ func-component/listing/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function ItemPrice ({price, currency}) {
-  if (currency === 'USD' ) {
+function ItemPrice({price, currency}) {
+  if (currency === 'USD') {
     return <p className='item-price'>${price}</p>
 
   } else if (currency === 'EUR') {
@@ -13,13 +13,16 @@ function ItemPrice ({price, currency}) {
   }
 }
 
-function ItemQuantity ({quantity}) {
+function ItemQuantity({quantity}) {
   if (quantity <= 10) {
     return <p className='item-quantity level-low'>{quantity} left</p>
+
   } else if (quantity > 10 && quantity <= 20) {
     return <p className='item-quantity level-medium'>{quantity} left</p>
+
   } else if (quantity > 20) {
     return <p className='item-quantity level-high'>{quantity} left</p>
+
   }
 }
 
@@ -37,8 +40,8 @@ function MyItem({item}) {
         </div>
         <div className='item-details'>
           <p className='item-title'>{item.title}</p>
-          <ItemPrice price = {item.price} currency = {item.currency_code} />
-          <ItemQuantity quantity = {item.quantity} />
+          <ItemPrice price={item.price} currency={item.currency_code} />
+          <ItemQuantity quantity={item.quantity} />
         </div>
       </div>
   );
@@ -46,7 +49,7 @@ function MyItem({item}) {
 
 function Listing({items}) {
   const lists = items.map((item) => {
-    return <MyItem item = {item} />;
+    return <MyItem item={item} />;
   });
 
   return <div className='item-list' >{lists}</div>;
@@ -61,7 +64,7 @@ fetch('https://neto-api.herokuapp.com/etsy')
   })
   .then(function(items) {
     ReactDOM.render(
-      <Listing items = {items} />,
+      <Listing items={items} />,
       document.getElementById('root')
     );
   })
