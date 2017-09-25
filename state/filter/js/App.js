74897ec8ea8d84a74fe8100c5c99db936f376c51
salnikovev <drsalnikovev@gmail.com>
2017-09-25 20:50:11 +0300

    filter done

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,34 @@
 'use strict'
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFilter: props.filters[0],
+      projectsFiltered: props.projects
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Toolbar
+          filters={this.props.filters}
+          selected={this.state.selectedFilter}
+          onSelectFilter={this.handleSetFilter} />
+        <Portfolio projects={this.state.projectsFiltered} />
+      </div>
+    );
+  }
+
+  handleSetFilter = (filter) => {
+    let projects = this.props.projects;
+
+    this.setState({
+      selectedFilter: filter,
+      projectsFiltered: projects.filter(project => filter === 'All' || project.category === filter )
+    });
+
+  };
+
+}
