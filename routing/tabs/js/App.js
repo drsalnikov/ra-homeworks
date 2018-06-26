4c77d7f72e8f1290e8444820e5c158948b4d29e1
salnikovev <drsalnikovev@gmail.com>
2018-06-26 18:49:56 +0300

    routing

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,20 @@
-
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="tabs">
+          <nav className="tabs__items">
+            <NavLink className="tabs__item" activeClassName="tabs__item-active" exact to="/">Рефераты</NavLink>
+            <NavLink className="tabs__item" activeClassName="tabs__item-active" exact to="/creator">Криэйтор</NavLink>
+            <NavLink className="tabs__item" activeClassName="tabs__item-active" exact to="/fortune">Гадалка</NavLink>
+          </nav>
+          <div className="tabs__content">
+            <Route path="/" exact component={Essay} />
+            <Route path="/creator" component={Creator} />
+            <Route path="/fortune" component={Fortune} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
